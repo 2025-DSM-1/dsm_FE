7d0b2caa094b65e2d34f11ac8b72b1b3b4526c82
limda00 <dylim7715@gmail.com>
2025-07-16 00:14:39 +0900

    feat :: instance 생성

---
+++ src/apis/index.ts
@@ -0,0 +1,52 @@
+import axios, { AxiosError } from "axios"
+import { getToken, setToken } from "./function/TokenManager";
+
+const BASEURL = import.meta.env.VITE_BASE_URL;
+
+export const instance = axios.create({
+  baseURL: BASEURL,
+  timeout: 10000,
+});
+
+export const refreshInstance = axios.create({
+  baseURL: BASEURL,
+  timeout: 10000
+})
+
+instance.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    if (axios.isAxiosError(error) && error.response) {
+      const originalRequest: any = error.config;
+
+      if (error.response.status === 401 && !originalRequest._retry && !originalRequest.url.includes('/auth/refresh')) {
+        originalRequest._retry = true;
+
+        try {
+          const refreshToken = getToken().refreshToken;
+
+          const res = await refreshInstance.get("/auth/refresh", {
+            headers: {
+              "authorization": `Bearer ${refreshToken}`,
+            },
+          });
+
+          const { access_token, refresh_token } = res.data;
+          setToken(access_token, refresh_token);
+
+          originalRequest.headers["authorization"] = `Bearer ${access_token}`;
+
+          return instance(originalRequest);
+        } catch (refreshError) {
+          window.location.href = "/login";
+          return Promise.reject(refreshError);
+        }
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+
+export default instance;
