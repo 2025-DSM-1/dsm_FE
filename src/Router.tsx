61c2e3d82bae0c9c28f5b37a287f074eba1f2f60
j02on <pjylove08@naver.com>
2025-07-16 11:22:24 +0900

    fix :: router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { Login, SignUp, CardDetail, BillList, Main, MyPage, Quiz } from './pages';
+import {
+  Login,
+  SignUp,
+  CardDetail,
+  BillList,
+  Main,
+  MyPage,
+  Quiz,
+} from './pages';
 import { RootLayout } from './layout';
 
 export const Router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Root</div>,
-  },
-  {
-    path: '/login',
     element: <Login />,
   },
   {
