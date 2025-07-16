e08b8b4587510ae7373ed43b29532d83fc6a2d64
limda00 <dylim7715@gmail.com>
2025-07-16 22:49:31 +0900

    feat :: 리스트 정렬

--- src/pages/BillList.tsx
+++ src/pages/BillList.tsx
@@ -83,6 +83,7 @@ export const BillList = () => {
 //   ]);
   const navigate = useNavigate();
   const [visibleCount, setVisibleCount] = useState(20);
+  const [sortSelected, setSortSelected] = useState<string>('가나다 순');
 
   const handleMore = () => {
     setVisibleCount((prev) => prev + 20);
@@ -106,11 +107,29 @@ export const BillList = () => {
           </ProcedureContainer>
         </TitleContainer>
         <PostCountContainer>
-          <PostCount>
-            총{' '}
-            <PostCount style={{ fontWeight: '600' }}>{data.length}</PostCount>개
-          </PostCount>
-          <div>
+          <PostHeader>
+            <PostCount>
+              총{' '}
+              <PostCount style={{ fontWeight: '600' }}>{datas.length}</PostCount>
+              개
+            </PostCount>
+            <SortWrapper>
+              <SortText
+                isSelected={sortSelected === '가나다 순'}
+                onClick={() => setSortSelected('가나다 순')}
+              >
+                가나다 순
+              </SortText>
+              <SortLine />
+              <SortText
+                isSelected={sortSelected === '최신제안 순'}
+                onClick={() => setSortSelected('최신제안 순')}
+              >
+                최신제안 순
+              </SortText>
+            </SortWrapper>
+          </PostHeader>
+          <TableContainer>
             <TabBarContainer>
               <TabTitle basis="43rem">법안</TabTitle>
               <TabTitle basis="9.5rem">제안일자</TabTitle>
@@ -130,7 +149,7 @@ export const BillList = () => {
                 />
               ))}
             </PostContainer>
-          </div>
+          </TableContainer>
         </PostCountContainer>
         {visibleCount < data.length && (
           <Button
@@ -143,7 +162,7 @@ export const BillList = () => {
           </Button>
         )}
       </ButtonContainer>
-    </Container>
+    </Container >
   );
 };
 
@@ -168,6 +187,11 @@ const PostCountContainer = styled.div`
   flex-direction: column;
   gap: 1.25rem;
 `;
+
+const PostHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
 
 const PostCount = styled.span`
   font-size: 1rem;
@@ -224,3 +248,28 @@ const Title = styled.div`
   font-size: 1.5rem;
   font-weight: 700;
 `;
+
+const SortWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`
+
+const SortText = styled.p<{ isSelected: boolean }>`
+  font-size: 16px;
+  font-weight: 500;
+  color: ${({ isSelected }) => (isSelected ? '#000000' : '#b0b0b0')};
+  cursor: pointer;
+`
+
+const SortLine = styled.div`
+  width: 1.5px;
+  height: 16px;
+  background-color: #000;
+`
+
+const TableContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`
