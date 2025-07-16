2007b962e64fb055e3fa20d9b90d15a953635a31
limda00 <dylim7715@gmail.com>
2025-07-16 09:15:14 +0900

    add :: Auth API

---
+++ src/apis/Auth.ts
@@ -0,0 +1,48 @@
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import instance from "./index";
+
+const path = '/auth';
+
+interface SignUpData {
+  name: string,
+  email: string,
+  authCode: string,
+  password: string,
+  passwordCheck: string
+}
+
+interface LoginData {
+  email: string,
+  password: string
+}
+
+// 회원가입
+export const useSignUp = () => {
+  return useMutation<void, AxiosError, SignUpData>({
+    mutationFn: async (formData) => {
+      await instance.post(`${path}/sign-up`, formData);
+    },
+    onSuccess: () => {
+      console.log("회원가입 성공");
+    },
+    onError: (error) => {
+      console.error(error.message);
+    }
+  })
+}
+
+// 로그인
+export const useLogin = () => {
+  return useMutation<void, AxiosError, LoginData>({
+    mutationFn: async (loginData) => {
+      await instance.post(`${path}/login`, loginData);
+    },
+    onSuccess: () => {
+      console.log("로그인 성공");
+    },
+    onError: (error) => {
+      console.error(error.message);
+    }
+  })
+}
